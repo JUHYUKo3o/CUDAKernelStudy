7c1b51af793b6d648f857acc58821907f6eed654
JUHYUKo3o <pju421@naver.com>
2020-10-21 01:38:37 +0900

    Sobel Laplacian 추가 그림저장 주석처리

--- Main.py
+++ Main.py
@@ -7,58 +7,67 @@ from CUDA_Kernal import convolution
 from imagecall import imagecall
 from select_kernel import ker_is
 
-impath = "CUDAKernelStudy\\Ryan.png"
+impath = "CUDAKernelStudy\\Calendar.png"
 bias = np.array([0])
 
 # 원본
 Data = imagecall(impath).img
-plt.subplot(2, 3, 1)
-plt.imshow(Data, cmap='gray')
-plt.title('Ryan')
-
-# Gaussian blur (7x7)
-Data = imagecall(impath).img
-kernel = ker_is(mode='Gaussian7').kernel
-for i in range(5):
-    Data = convolution(Data,kernel,bias,mode='same').conv_2()
-plt.subplot(2, 3, 2)
-plt.imshow(Data, cmap='gray')
-plt.title('Gaussian7')
-
-# Gaussian blur (15x15)
-Data = imagecall(impath).img
-kernel = ker_is(mode='Gaussian15').kernel
-for i in range(5):
-    Data = convolution(Data,kernel,bias,mode='same').conv_2()
-plt.subplot(2, 3, 3)
-plt.imshow(Data, cmap='gray')
-plt.title('Gaussian15')
+# plt.imshow(Data, cmap='gray')
+# plt.title('Original')
+# plt.imsave('CUDAKernelStudy\\Original.png', Data, cmap='gray')
 
 # Motion blur (Updown)
 Data = imagecall(impath).img
 kernel = ker_is(mode='UpDown').kernel
 for i in range(30):
     Data = convolution(Data,kernel,bias,mode='same').conv_2()
-plt.subplot(2, 3, 4)
-plt.imshow(Data, cmap='gray')
-plt.title('UpDown')
+# plt.imsave('CUDAKernelStudy\\UpDown.png', Data, cmap='gray')
 
 # Motion blur (LeftRight)
 Data = imagecall(impath).img
 kernel = ker_is(mode='LeftRight').kernel
 for i in range(30):
     Data = convolution(Data,kernel,bias,mode='same').conv_2()
-plt.subplot(2, 3, 5)
-plt.imshow(Data, cmap='gray')
-plt.title('LeftRight')
+# plt.imsave('CUDAKernelStudy\\LeftRight.png', Data, cmap='gray')
 
-# Motion blur (High-passfilter)
+# Gaussian blur (3x3)
+Data = imagecall(impath).img
+kernel = ker_is(mode='Gaussian').kernel
+for i in range(5):
+    Data = convolution(Data,kernel,bias,mode='same').conv_2()
+# plt.imsave('CUDAKernelStudy\\Gaussian.png', Data, cmap='gray')
+
+# Gaussian blur (5x5)
+Data = imagecall(impath).img
+kernel = ker_is(mode='Gaussian5').kernel
+for i in range(5):
+    Data = convolution(Data,kernel,bias,mode='same').conv_2()
+# plt.imsave('CUDAKernelStudy\\Gaussian5.png', Data, cmap='gray')
+
+# Bilateral blur
+Data = imagecall(impath).img
+kernel = ker_is(mode='Bilateral').kernel
+for i in range(5):
+    Data = convolution(Data,kernel,bias,mode='same').conv_2()
+# plt.imsave('CUDAKernelStudy\\Bilateral.png', Data, cmap='gray')
+
+# Laplacian blur
 Data = imagecall(impath).img
 kernel = ker_is(mode='Laplacian').kernel
 for i in range(1):
     Data = convolution(Data,kernel,bias,mode='same').conv_2()
-plt.subplot(2, 3, 6)
-plt.imshow(Data, cmap='gray')
-plt.title('Laplacian')
+# plt.imsave('CUDAKernelStudy\\Laplacian.png', Data, cmap='gray')
 
-plt.show()
+# Sobelx blur
+Data = imagecall(impath).img
+kernel = ker_is(mode='Sobelx').kernel
+for i in range(1):
+    Data = convolution(Data,kernel,bias,mode='same').conv_2()
+# plt.imsave('CUDAKernelStudy\\Sobelx.png', Data, cmap='gray')
+
+# Sobely blur
+Data = imagecall(impath).img
+kernel = ker_is(mode='Sobely').kernel
+for i in range(1):
+    Data = convolution(Data,kernel,bias,mode='same').conv_2()
+# plt.imsave('CUDAKernelStudy\\Sobely.png', Data, cmap='gray')
